72d68ce5ded6c306519366c6f4957d06b1a5ee8b
purpul!!! <114886575+purpul-o@users.noreply.github.com>
2025-05-18 00:09:24 +0200

    Create Configuration.lua

---
+++ Source/Client/Module Scripts/Configuration.lua
@@ -0,0 +1,19 @@
+return {
+	Terrain = {
+		Terrain_Size = 3000,
+		Part_Size = 10
+	},
+	
+	Noise = {
+		Resolution = 30,
+		Frequency = 1,
+		Amplitude = 30
+	},
+	
+	Other = {
+		Actors = 128,
+		
+		Top_Lerp = Color3.fromRGB(255, 255, 100),
+		Bottom_Lerp = Color3.fromRGB(100, 100, 40)
+	}
+}
